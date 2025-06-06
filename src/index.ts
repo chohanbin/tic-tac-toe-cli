f4dbc08d574c3262c82c01c9173200bf7f8439e8
Hanbin Cho <5660356+chohanbin@users.noreply.github.com>
2025-06-06 13:41:00 -0400

    chore: setup pre-commit check for linting, formatting issue with biome

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 type Note = {
   message: string;
   id: number;
-}
+};
 
 const note: Note = {
   message: "Hello World!",
-  id: 123
-}
+  id: 123,
+};
 
 console.log(`a note ${note.id} says ${note.message}`);
